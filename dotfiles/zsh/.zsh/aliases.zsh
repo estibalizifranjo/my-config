15c9e04e40242d655cccd1d9e4eb31b83522993e
Daniel Otero <danielotero@users.noreply.github.com>
2016-04-30 03:08:27 +0200

    Update zsh aliases

--- dotfiles/zsh/.zsh/aliases.zsh
+++ dotfiles/zsh/.zsh/aliases.zsh
@@ -5,6 +5,4 @@ alias scp="scp -r"
 alias fuck='sudo $(fc -ln -1)'
 alias vi="vim"
 alias wget="curl -O --retry 999 --retry-max-time 0 -L -C -"
-alias ggraph='git log --oneline --graph --decorate --all --date-order'
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
+alias gitv='git log --oneline --graph --decorate --all --date-order'
