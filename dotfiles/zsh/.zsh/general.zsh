e96639f0c9da304f0bdc4eb3e322e61aad654e36
Daniel Otero <danielotero@users.noreply.github.com>
2018-10-24 13:29:54 +0200

    Improve zsh 'time' built-in report

--- dotfiles/zsh/.zsh/general.zsh
+++ dotfiles/zsh/.zsh/general.zsh
@@ -1,2 +1,11 @@
 unsetopt beep
 
+TIMEFMT=$'---\n%J\n---\n'\
+$'%U user, %S system, %P cpu, %*E total\n'\
+$'avg shared (code):         %X KB\n'\
+$'avg unshared (data/stack): %D KB\n'\
+$'total (sum):               %K KB\n'\
+$'max memory:                %M MB\n'\
+$'page faults from disk:     %F\n'\
+$'other page faults:         %R'
+
